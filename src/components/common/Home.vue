381aa7f542de71ed6077251d9b56976947dbda73
zhaoyue <1553840706@qq.com>
2020-07-02 10:58:48 +0800

    input框前置图标不显示

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -1,6 +1,6 @@
 <template>
-    <el-row class="container">
-        {{123333333}}
+<div class="container">
+    <el-row >
         <el-col :span="24" class="header">
             <div class="logo header-left">
                 <img src="../../assets/logo.png" alt="">
@@ -15,23 +15,42 @@
                         <span></span>
                     </div>
                     <div class="userinfo">
-
+                        <el-dropdown>
+                            <span><i></i></span>
+                             <el-dropdown-menu>
+                                 <el-dropdown-item></el-dropdown-item>
+                             </el-dropdown-menu>
+                        </el-dropdown>
                     </div>
                 </div>
             </div>
         </el-col>
+        <el-col :span="24" class="main">
+            <aside class="menu-collapsed">
+                <div>
+                    <el-input  type="text" placeholder="输入关键字，自动搜索" prefix-icon="el-icon-search"></el-input>
+                    <div class="tools">
+                        <i class="fa fa-align-justify"></i>
+                    </div>    
+                </div>
+                <v-sidebar></v-sidebar>
+            </aside>
+        </el-col>
     </el-row>
+</div>
 </template>
 <script>
-import topmenu from "./topmenu.vue"
+import topmenu from './topmenu.vue'
+import vSidebar from './Sidebar.vue'
 export default {
     data(){
        return {
-           logoName:"新一代电费结算"
+           logoName:"新一代电费结算应用网站"
        }
     },
-    commponents:{
-        topmenu
+    components:{
+        topmenu,
+        vSidebar
     }
 }
 </script>
@@ -39,14 +58,38 @@ export default {
     .container{
         position: absolute;
         top:0px;
-        bottom:0pxq;
+        bottom:0px;
         width:100%;
     }
     .container .header{
         min-width:1270px;
         height:46px;
-        background: #00f;
+        background: #444;
         line-height:46px;
         color:#fff;
+    }
+    .container .header .logo{
+        width:250px;
+        height:46px;
+        font-size:18px;
+        padding-left:10px;
+    }
+    .container .header .logo img{
+        width:34px;
+        height:34px;
+        float: left;
+        margin:5px 10px 0px 0px;
+    }
+    .container .main .menu-collapsed{
+        display:flex;
+        position:absolute;
+        top:46px;
+        overflow: auto;
+        bottom: 0;
+        height: calc(100vh - 46px);
+    }
+    .container .main .menu-collapsed{
+        width:200px;
+        background-color:skyblue;
     }
 </style>
